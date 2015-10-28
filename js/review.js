0947f7d594cff94a6d373485f2ed30c50d1db04f
Sam Wahbeh <stwahbeh@gmail.com>
2015-10-27 17:39:30 -0700

    updating

---
+++ js/review.js
@@ -0,0 +1,54 @@
+//Initialize Parse
+Parse.Initialize('0AwLRhdzdREF2SIsk9ApySV03rEyzgt5uKdAKtq7', 'obla7Mim9lU8uNAeZqAQLdL5CBGsl9NWbvyxpVT5');
+
+var Review = Parse.Object.extend('Review');
+
+ $('div').raty();
+
+//Click event when form is submitted
+
+$("#button").submit(function() {
+
+	//Save form fields 
+	var title = $('title').val();
+	var review = $('review').val();
+
+	//Set a property of your new instance equal to data entered
+
+
+
+	//Save instance back to your data
+
+});
+
+parseInt(string, radix);
+
+
+//Function to get data
+var getData = function() {
+
+	var query = new Parse.Query(Review);
+
+
+});
+
+
+
+var buildList = function(data) {
+
+
+
+});
+
+
+var addItem= function(item) {
+
+
+
+});
+
+$('#rating').raty({ number: 10 });
+
+$('div').raty('click', number); 
+
+$('div').raty({ score: 3 });
