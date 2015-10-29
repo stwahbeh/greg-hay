3e62c38766435994fcb08b13cba639df7b360c6b
Sam Wahbeh <stwahbeh@gmail.com>
2015-10-28 17:35:11 -0700

    updating files

--- js/review.js
+++ js/review.js
@@ -1,54 +1,108 @@
 //Initialize Parse
-Parse.Initialize('0AwLRhdzdREF2SIsk9ApySV03rEyzgt5uKdAKtq7', 'obla7Mim9lU8uNAeZqAQLdL5CBGsl9NWbvyxpVT5');
+Parse.initialize('obla7Mim9lU8uNAeZqAQLdL5CBGsl9NWbvyxpVT5', '0AwLRhdzdREF2SIsk9ApySV03rEyzgt5uKdAKtq7');
 
 var Review = Parse.Object.extend('Review');
 
- $('div').raty();
+ 
 
 //Click event when form is submitted
 
-$("#button").submit(function() {
+// function validateForm() {
+//     var x = document.forms["myForm"]["fname"].value;
+//     if (x == null || x == "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }
+// }
 
-	//Save form fields 
-	var title = $('title').val();
-	var review = $('review').val();
 
+//$("form").submit(function() {
+var complete = function() {
+	var review = new Review();
+
+	
 	//Set a property of your new instance equal to data entered
+	$(this).find('input').each(function() {
+		review.set($(this).attr('id'), $(this).val());
+		console.log($(this).attr('id') + " " + $(this).val());
+		$(this).val('');
+	});
 
-
+	//Sets property of stars
+	var rate = parseInt($("#rate").raty('score'));
+	console.log("score is " + rate);
+	review.set('rate',rate); 
+	
 
 	//Save instance back to your data
-
-});
-
-parseInt(string, radix);
-
+	review:save(null,{
+		success:getData	
+	})
+	console.log("didnt print");
+};
 
 //Function to get data
 var getData = function() {
 
 	var query = new Parse.Query(Review);
 
+	query.find({
+		success:function(results) {
+			buildList(results)
+		}
+	})
 
-});
+
+};
 
 
 
 var buildList = function(data) {
+	//Empty our ordered list
+	$('ol').empty()
+
+	data.forEach(function(d){
+		addItem(d);
+	})
+};
 
 
 
-});
-
-
-var addItem= function(item) {
 
 
 
-});
+// // var addItem= function(item) {
+// // 	var stars = item.get('rate.raty')
+// // 	var title = item.get('title')
+// // 	var review = item.get('review')
 
-$('#rating').raty({ number: 10 });
+// // 	//Append statements
 
-$('div').raty('click', number); 
+// // 	var div = $('<div class="container"> 
+// // 		<p>	' + stars + title + </p>
+// // 		<p> '+ review + '</p>'
+// // 		'<h4> "3 out of 4 found this review helpful"</h4>'
+// // 	</div>')
 
-$('div').raty({ score: 3 });
+
+// // 	$('ol').Append(li)
+
+
+
+// // };
+
+
+
+// //adds average rating
+
+$(function() {
+  getData();  
+  $('#rate').raty();
+  $('#average').raty({ readOnly: true, score: 3 });
+  $('form').submit(function() {
+  	complete();
+  	return false;
+  })
+});	
+
+
