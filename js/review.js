2ab26090629a4c1d9859400c4981c6f3f477e7f5
Sam Wahbeh <stwahbeh@gmail.com>
2015-10-29 13:30:21 -0700

    need to fix buttons

--- js/review.js
+++ js/review.js
@@ -1,9 +1,11 @@
+
 //Initialize Parse
 Parse.initialize('obla7Mim9lU8uNAeZqAQLdL5CBGsl9NWbvyxpVT5', '0AwLRhdzdREF2SIsk9ApySV03rEyzgt5uKdAKtq7');
 
 var Review = Parse.Object.extend('Review');
 
- 
+var count = 0;
+var totalRating = 0;
 
 //Click event when form is submitted
 
@@ -18,80 +20,113 @@ var Review = Parse.Object.extend('Review');
 
 
 var complete = function() {
-	var review = new Review();
-	
-	//Set a property of your new instance equal to data entered
-	$(this).find('input').each(function() {
-		review.set($(this).attr('id'), $(this).val());
-		console.log($(this).attr('id') + " " + $(this).val());
-		$(this).val('');
-	});
+    var review = new Review();
+    //console.log("1");
+    //Set a property of your new instance equal to data entered
+    $('form').find('input').each(function() {
+        review.set($(this).attr('id'), $(this).val());
+        $(this).val('');
+    });
 
-	//Sets property of stars
-	var rate = parseInt($("#rate").raty('score'));
-	console.log("score is " + rate);
-	review.set('rate',rate); 	
+    //Sets property of stars
+    var rate = parseInt($("#rate").raty('score'))
+    if(rate == null){
+        rate = 0;
+    }
 
-	//Save instance back to your data
-	review.save(null,{
-		success:getData	
-	})
-	console.log("didnt print");
+    review.set('rate', rate);
+    $('#rate').raty({score:0});
+
+    review.set('upvotes', 0);
+    review.set('downvotes', 0);
+
+    count++;
+    totalRating += rate;
+
+
+    //Save instance back to your data
+    review.save(null, {
+        success: getData
+    })
+    
+   
+
 };
 
 //Function to get data
 var getData = function() {
-
-	var query = new Parse.Query(Review);
-
-	query.find({
-		success:function(results) {
-			buildList(results)
-		}
-	})
+    //console.log("2");
+    var query = new Parse.Query(Review);   
 
 
+    query.find({
+        success: function(results) {
+            buildList(results)
+        }
+    })
 };
 
 
-
- var buildList = function(data) {
-	//Empty our ordered list
-	$('ol').empty()
-
-	data.forEach(function(d){
-		addItem(d);
-	})
+var buildList = function(data) {  
+    $('.list').empty()
+    data.forEach(function(d){
+        addItem(d);
+    })
+    var averageRating = (totalRating/count)
+    $("#average").raty({score: averageRating, readOnly: true});
 };
 
-var addItem= function(item) {
-	var stars = item.get('rate.raty')
-	var title = item.get('title')
-	var review = item.get('review')
+var addItem = function(item) {
+   //console.log('7');
+    var stars = item.get('rate')
+    var title = item.get('title')
+    var review = item.get('review')
+    //Append statements
+    count++;
+    totalRating += stars;
 
-	//Append statements
+    var ratyDiv = $('<span></span>')
 
-	var div = $('<div class="container"> 
-		<p>	' + stars + title + </p>
-		<p> '+ review + '</p>'
-		'<h4> "3 out of 4 found this review helpful"</h4>'
-	</div>')
+    var div = $('<div class="container list"></div>')
+    div.append(ratyDiv);
 
 
-	$('ol').Append(li)
+    var body = $('<h3 id="bleh">'+ "  " + title + "  " +'</h3><a><i class="fa fa-thumbs-down"></i></a><a><i class="fa fa-thumbs-up"></i></a><h4>' + review + '</h4><h6> 3 out of 5 people found this helpful</h6></div>')
+    div.append(body);
+    
+    var dest = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
+    div.append(dest);
+    console.log('8');
+    dest.click(function() {
+        item.destroy({
+            success: getData()
+
+        })
+
+    })
+    
+       ratyDiv.raty({
+        readOnly: true,
+        score: stars,
+        size: 12
+    });
+
+    $('#reviews').append(div)
 
 };
+
+//I put this here but I dont know why?
+//getData()
 
 // //adds average rating
 
 $(function() {
-  getData();  
-  $('#rate').raty();
-  $('#average').raty({ readOnly: true, score: 3 });
-  $('form').submit(function() {
-  	complete();
-  	return false;
-  })
-});	
-
-
+    console.log('9')
+    getData()
+    console.log('10')
+    $('#rate').raty();
+    $('form').submit(function() {
+        complete();
+        return false;
+    })
+});
